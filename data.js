194b0e724b7d13d28531d0c5eb8cb9f54eda640f
Ian Mcdonald <iangregoryjames@gmail.com>
2016-10-04 00:56:40 -0400

    something something

--- data.js
+++ data.js
@@ -8,14 +8,30 @@ const Signs = [
     ruler: 'Mars',
     color: 'red',
     numbers: [1, 9],
-    startDate: {month: 'March', day: 21},
-    endDate: {month: 'April', day: 20},
+    startDate: {
+      month: 'March',
+      day: 21
+    },
+    endDate: {
+      month: 'April',
+      day: 20
+    },
     description: 'As the first sign in the zodiac, the presence of Aries almost always marks the beginning of something energetic and turbulent.They are continuously looking for dynamic, speed and competition. They are always first in everything - from work to social gatherings. Thanks to its ruling planet Mars, Aries is one of the most active zodiac signs. People born under the Aries sign, are meant to emphasize the search for answers to personal and metaphysical questions. This is the biggest feature of this incarnation.',
     strengths: [
-      'courageous', 'determined', 'confident', 'enthusiastic', 'optimistic', 'honest', 'passionate'
+      'courageous',
+      'determined',
+      'confident',
+      'enthusiastic',
+      'optimistic',
+      'honest',
+      'passionate'
     ],
     weaknesses: [
-      'impatient', 'moody', 'short-tempered', 'impulsive', 'aggressive'
+      'impatient',
+      'moody',
+      'short-tempered',
+      'impulsive',
+      'aggressive'
     ]
   },
 
@@ -28,14 +44,27 @@ const Signs = [
     ruler: 'Venus',
     color: 'blue',
     numbers: [2, 4, 6, 11, 20, 29, 37, 47, 56],
-    startDate: {month: 'April', day: 21},
-    endDate: {month: 'May', day: 21},
+    startDate: {
+      month: 'April',
+      day: 21
+    },
+    endDate: {
+      month: 'May',
+      day: 21
+    },
     description: 'Powerful and reliable, Taurus is the first when it comes to harvesting the fruits of his labor. They love everything that is good and beautiful, and they are often surrounded by material pleasures. People born under the Taurus sign are very sensual and tactile. Touch is extremely important for them, both in business and in romance. Stable and conservative, Taurus is among the most reliable signs of the zodiac. Stubbornness is a trait that is forcing him to expel things to the end, in order to comply with the standards.',
     strengths: [
-      'reliable', 'patient', 'practical', 'devoted', 'responsible', 'stable'
+      'reliable',
+      'patient',
+      'practical',
+      'devoted',
+      'responsible',
+      'stable'
     ],
     weaknesses: [
-      'stubborn', 'possessive', 'uncompromising'
+      'stubborn',
+      'possessive',
+      'uncompromising'
     ]
   },
 
@@ -48,14 +77,25 @@ const Signs = [
     ruler: 'Mercury',
     color: 'green',
     numbers: [3, 8, 12, 23],
-    startDate: {month: 'May', day: 22},
-    endDate: {month: 'June', day: 21},
+    startDate: {
+      month: 'May',
+      day: 22
+    },
+    endDate: {
+      month: 'June',
+      day: 21
+    },
     description: 'Expressive and quick-witted, Gemini represents two different sides of personality and you will never be sure with whom you will face. Gemini can be sociable,communicative and ready for fun, while on the other hand it can be very serious, thoughtful, restless and even indecisive.',
     strengths: [
-      'gentle', 'affectionate', 'curious', 'adaptable'
+      'gentle',
+      'affectionate',
+      'curious',
+      'adaptable'
     ],
     weaknesses: [
-      'nervous', 'inconsistent', 'indecisive'
+      'nervous',
+      'inconsistent',
+      'indecisive'
     ]
   },
 
@@ -68,14 +108,29 @@ const Signs = [
     ruler: 'Moon',
     color: 'orange',
     numbers: [2, 7, 11, 16, 20, 25],
-    startDate: {month: 'June', day: 22},
-    endDate: {month: 'July', day: 22},
+    startDate: {
+      month: 'June',
+      day: 22
+    },
+    endDate: {
+      month: 'July',
+      day: 22
+    },
     description: 'Deeply intuitive and sentimental, Cancer can be one of the most challenging Zodiac signs to get to know. Cancer is very emotional and sensitive, and they care about family and home. Cancer is sympathetic and is very attached to the people who surround him. People born under the Cancer sign are very loyal and empathetic people, able to empathize with your pain and suffering.',
     strengths: [
-      'tenacious', 'highly imaginative', 'loyal', 'emotional', 'sympathetic', 'persuasive'
+      'tenacious',
+      'highly imaginative',
+      'loyal',
+      'emotional',
+      'sympathetic',
+      'persuasive'
     ],
     weaknesses: [
-      'moody', 'pessimistic', 'suspicious', 'manipulative', 'insecure'
+      'moody',
+      'pessimistic',
+      'suspicious',
+      'manipulative',
+      'insecure'
     ]
   },
 
@@ -88,14 +143,29 @@ const Signs = [
     ruler: 'Sun',
     color: 'gold',
     numbers: [1, 4, 10, 13, 19, 22],
-    startDate: {month: 'July', day: 23},
-    endDate: {month: 'August', day: 23},
+    startDate: {
+      month: 'July',
+      day: 23
+    },
+    endDate: {
+      month: 'August',
+      day: 23
+    },
     description: 'People born under the sign of Leo are natural born leaders. They are dramatic, creative self-confident, dominant and extremely difficult to resist. They can achieve anything they want, whether it\'s about work or time spent will family and friends.',
     strengths: [
-      'creative', 'passionate', 'generous', 'warm-hearted', 'cheerful', 'humorous'
+      'creative',
+      'passionate',
+      'generous',
+      'warm-hearted',
+      'cheerful',
+      'humorous'
     ],
     weaknesses: [
-      'arrogant', 'stubborn', 'self-centered', 'lazy', 'inflexible'
+      'arrogant',
+      'stubborn',
+      'self-centered',
+      'lazy',
+      'inflexible'
     ]
   },
   
@@ -108,14 +178,26 @@ const Signs = [
     ruler: 'Mercury',
     color: 'yellow',
     numbers: [5, 14, 23, 32, 41, 50],
-    startDate: {month: 'August', day: 24},
-    endDate: {month: 'September', day: 22},
+    startDate: {
+      month: 'August',
+      day: 24
+    },
+    endDate: {
+      month: 'September',
+      day: 22
+    },
     description: 'Virgos are always paying attention to smallest details and their deep sense of humanity makes them one of the most careful signs of the zodiac. Their methodical approach to life ensures that nothing is left to chance. Virgos are often tender but also very careful.',
     strengths: [
-      'loyal', 'analytical', 'kind', 'hardworking', 'practical'
+      'loyal',
+      'analytical',
+      'kind',
+      'hardworking',
+      'practical'
     ],
     weaknesses: [
-      'shyness', 'worry', 'overly critical'
+      'shyness',
+      'worry',
+      'overly critical'
     ]
   },
 
@@ -128,14 +210,27 @@ const Signs = [
     ruler: 'Venus',
     color: 'blue',
     numbers: [6, 15, 24, 33, 42, 51, 60],
-    startDate: {month: 'September', day: 23},
-    endDate: {month: 'October', day: 22},
+    startDate: {
+      month: 'September',
+      day: 23
+    },
+    endDate: {
+      month: 'October',
+      day: 22
+    },
     description: 'People born under the sign of Libra are peaceful and fair, and they hate being alone. Partnership is very important for Libra -born, and with their victorious mentality and cooperation, they cannot stand to be alone. The Libra is an Air sign, with expressed intellect and a keen mind. They can be inspired by good books, insurmountable discussions and interesting people.',
     strengths: [
-      'cooperative', 'diplomatic', 'gracious', 'fair-minded', 'social'
+      'cooperative',
+      'diplomatic',
+      'gracious',
+      'fair-minded',
+      'social'
     ],
     weaknesses: [
-      'indecisive', 'avoids confrontation', 'grudgeful', 'self-pitying'
+      'indecisive',
+      'avoids confrontation',
+      'grudgeful',
+      'self-pitying'
     ]
   },
 
@@ -149,14 +244,27 @@ const Signs = [
     ruler: 'Mars',
     color: 'red',
     numbers: [9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
-    startDate: {month: 'October', day: 23},
-    endDate: {month: 'November', day: 21},
+    startDate: {
+      month: 'October',
+      day: 23
+    },
+    endDate: {
+      month: 'November',
+      day: 21
+    },
     description: 'Scorpio-born are passionate and assertive people. They are determined and decisive, and will research until they find out the truth. Scorpio is a great leader, always aware of the situation and also features prominently in resourcefulness.',
     strengths: [
-      'resourceful', 'brave', 'passionate', 'stubborn', 'loyal'
+      'resourceful',
+      'brave',
+      'passionate',
+      'stubborn',
+      'loyal'
     ],
     weaknesses: [
-      'distrusting', 'jealous', 'secretive', 'violent'
+      'distrusting',
+      'jealous',
+      'secretive',
+      'violent'
     ]
   },
 
@@ -169,14 +277,24 @@ const Signs = [
     ruler: 'Jupiter',
     color: 'violet',
     numbers: [3, 12, 21, 30],
-    startDate: {month: 'November', day: 22},
-    endDate: {month: 'December', day: 21},
+    startDate: {
+      month: 'November',
+      day: 22
+    },
+    endDate: {
+      month: 'December',
+      day: 21
+    },
     description: 'Curious and energetic, Sagittarius is one of the biggest travelers among all zodiac signs. Their open mind and philosophical view motivates them to wander around the world in search of the meaning of life.',
     strengths: [
-      'generous', 'idealistic', 'humourful'
+      'generous',
+      'idealistic',
+      'humourful'
     ],
     weaknesses: [
-      'overpromises', 'impatient', 'undiplomatic'
+      'overpromises',
+      'impatient',
+      'undiplomatic'
     ]
   },
 
@@ -191,14 +309,25 @@ const Signs = [
     ruler: 'Saturn',
     color: 'brown',
     numbers: [1, 4, 8, 10, 13, 17, 19, 22, 26],
-    startDate: {month: 'December', day: 22},
-    endDate: {month: 'January', day: 20},
+    startDate: {
+      month: 'December',
+      day: 22
+    },
+    endDate: {
+      month: 'January',
+      day: 20
+    },
     description: 'When it comes to professionalism and traditional values, Capricorn is the first. Capricorn is practical and is considered to be the most serious sign of the zodiac, who possess an independence that enables significant progress both on the personal level and in business.',
     strengths: [
-      'responsible', 'disciplined', 'self-control', 'managerial'
+      'responsible',
+      'disciplined',
+      'self-control',
+      'managerial'
     ],
     weaknesses: [
-      'know-it-all', 'unforgiving', 'condescending'
+      'know-it-all',
+      'unforgiving',
+      'condescending'
     ]
   },
 
@@ -210,14 +339,25 @@ const Signs = [
     ruler: 'Uranus',
     color: 'blue',
     numbers: [4, 8, 13, 17, 22, 26],
-    startDate: {month: 'January', day: 21},
-    endDate: {month: 'February', day: 19},
+    startDate: {
+      month: 'January',
+      day: 21
+    },
+    endDate: {
+      month: 'February',
+      day: 19
+    },
     description: 'Aquarius-born are shy and quiet , but on the other hand they can be eccentric and energetic. However, in both cases, they are deep thinkers and highly intellectual people who love helping others. They are able to see without prejudice, on both sides, which makes them people who can easily solve problems.',
     strengths: [
-      'progressive', 'original', 'independent', 'humanitarian'
+      'progressive',
+      'original',
+      'independent',
+      'humanitarian'
     ],
     weaknesses: [
-      'temperamental', 'uncompromising', 'aloof'
+      'temperamental',
+      'uncompromising',
+      'aloof'
     ]
   },
 
@@ -230,14 +370,27 @@ const Signs = [
     ruler: 'Neptune',
     color: 'purple',
     numbers: [3, 7, 12, 16, 21, 25, 30, 34, 43, 52],
-    startDate: {month: 'February', day: 20},
-    endDate: {month: 'March', day: 20},
+    startDate: {
+      month: 'February',
+      day: 20
+    },
+    endDate: {
+      month: 'March',
+      day: 20
+    },
     description: 'Pisces are very friendly, so they often find themselves in a company of very different people. Pisces are selfless, they are always willing to help others, without hoping to get anything back.',
     strengths: [
-      'compassionate', 'artistic', 'intuitive', 'gentle', 'wise', 'musical'
+      'compassionate',
+      'artistic',
+      'intuitive',
+      'gentle',
+      'wise',
+      'musical'
     ],
     weaknesses: [
-      'fearful', 'overly trusting', 'sad'
+      'fearful',
+      'overly trusting',
+      'sad'
     ]
   }
 
